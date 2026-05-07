446d9b58574b31dc0c334bbc478c1a4a5b0a6b7f
akhildev <aakhildevk@gmail.com>
2026-05-07 13:32:41 +0530

    Add MainApp class with success message

---
+++ app.java
@@ -0,0 +1,8 @@
+package org.demo;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+        System.out.println("CI/CD pipeline executed successfully. Artifact published to GitHub Packages.");
+    }
+}
